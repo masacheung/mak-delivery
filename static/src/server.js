29b4beb0232c8703e2394a74d5339af23c8444d6
Masa <cheung.masa@gmail.com>
2025-02-14 21:09:50 -0500

    Updated server.js index.html path

--- static/src/server.js
+++ static/src/server.js
@@ -4,12 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "build")));
+// Serve static files from the correct build directory
+app.use(express.static(path.join(__dirname, "static", "build")));
 
 // Handle React routing, return index.html for all unknown routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "static", "build", "index.html"));
 });
 
 // Start the server
