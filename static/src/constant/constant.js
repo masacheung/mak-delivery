089540f133c2be83af85f9b74dc8b7ff6667d826
Masa <cheung.masa@gmail.com>
2025-02-27 17:36:16 -0500

    Removed react logo and added query by the pick up date

---
+++ static/src/constant/constant.js
@@ -0,0 +1,27 @@
+const PICK_UP_LOCATION = [
+    "Fort Lee 540 Main St",
+    "Hackensack 99 Ranch",
+    "Tenafly - 165 Grove St, Tenafly, NJ 07670",
+    "Weehawken - 150 Henley Place",
+    "Weehawken - 9 Ave at Port Imperial",
+    "900 Madison St, Hoboken, NJ 07030",
+    "Möge Tea - 2029 Lemoine Ave #102, Fort Lee, NJ 07024",
+    "Jersey City - Canopy 159 Morgan St",
+    "Jersey City - 1 Shorn Ln",
+    "Jersey City - 155 Bay St",
+    "JSQ - Overlook Flat",
+    "Ridgewood"
+];
+
+const RESTAURANT_NAME = {
+    1: "Tasty Moment",
+    2: "港茶巷 HK ALLEY",
+    3: "雲吞佳",
+    4: "S&Y Mini HotPot 蜀世冒菜",
+    5: "98K",
+    6: "葛师傅",
+    7: "Spice 24",
+    8: "Meetu"
+};
+
+export { PICK_UP_LOCATION, RESTAURANT_NAME };
