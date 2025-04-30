c5b5f2ef0341b8ff29c9002fc056149d8b9b0e33
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2025-04-29 23:08:47 -0400

    Update meeTu.js

--- static/src/constant/restaurants/meeTu.js
+++ static/src/constant/restaurants/meeTu.js
@@ -5,7 +5,7 @@ const MEE_TU = {
         { id: 81, name: '招牌生椰水', price:"SP",
           options: {
             optionSize: {
-                name: "Size", choices: ["M $7.50", "L $8.50"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $7.99", "L $8.99"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -33,7 +33,7 @@ const MEE_TU = {
         { id: 83, name: '玫瑰生椰水', price:"SP",
           options: {
             optionSize: {
-                name: "Size", choices: ["M $7.50", "L $8.50"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $8.25", "L $9.25"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -61,7 +61,7 @@ const MEE_TU = {
         { id: 85, name: '招牌生椰奶', price:"SP",
           options: {
             optionSize: {
-                name: "Size", choices: ["M $7.50", "L $8.50"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $7.99", "L $8.99"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -89,7 +89,7 @@ const MEE_TU = {
         { id: 87, name: '芒果生椰', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $7.75", "L $8.75"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $8.29", "L $9.29"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -117,7 +117,7 @@ const MEE_TU = {
         { id: 89, name: '斯里蘭卡奶茶', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.45", "L $6.45"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $5.95", "L $6.95"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -145,7 +145,7 @@ const MEE_TU = {
         { id: 811, name: '白蘭奶綠', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.45", "L $6.45"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $5.95", "L $6.95"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -173,7 +173,7 @@ const MEE_TU = {
         { id: 813, name: '栀香奶綠', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.45", "L $6.45"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $5.95", "L $6.95"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -201,7 +201,7 @@ const MEE_TU = {
         { id: 815, name: '山茶花奶烏', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.45", "L $6.45"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $5.95", "L $6.95"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -229,7 +229,7 @@ const MEE_TU = {
         { id: 817, name: '茉莉奶綠', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.45", "L $6.45"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $6.45", "L $7.45"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -257,7 +257,7 @@ const MEE_TU = {
         { id: 819, name: '玫瑰奶茶', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.45", "L $6.75"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $6.25", "L $7.25"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -285,7 +285,7 @@ const MEE_TU = {
         { id: 821, name: '海鹽奶綠', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.45", "L $6.45"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $6.25", "L $7.25"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -313,7 +313,7 @@ const MEE_TU = {
         { id: 823, name: '斯里蘭卡奶茶No Creamer Powder', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.95", "L $6.95"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $6.45", "L $7.45"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -341,7 +341,7 @@ const MEE_TU = {
         { id: 825, name: '白蘭奶綠No Creamer Powder', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.95", "L $6.95"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $6.45", "L $7.45"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -369,7 +369,7 @@ const MEE_TU = {
         { id: 827, name: '栀香奶綠No Creamer Powder', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.95", "L $6.95"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $6.45", "L $7.45"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -397,7 +397,7 @@ const MEE_TU = {
         { id: 829, name: '山茶花奶烏No Creamer Powder', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.95", "L $6.95"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $6.45", "L $7.45"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -425,7 +425,7 @@ const MEE_TU = {
         { id: 831, name: '茉莉奶綠No Creamer Powder', price:"SP",
         options: {
             optionSize: {
-                name: "Size", choices: ["M $5.95", "L $6.95"], selectedOptions: [], limit: 1, adjustable: true
+                name: "Size", choices: ["M $6.45", "L $7.45"], selectedOptions: [], limit: 1, adjustable: true
             },
              option1:
                   {
@@ -450,7 +450,7 @@ const MEE_TU = {
                       choices: [ "正常冰", "少冰", "去冰"], selectedOptions: [], limit: 1
                   },
             }},
-        { id: 834, name: '手打檸檬紅茶(L)', price:6.95,
+        { id: 834, name: '手打檸檬紅茶(L)', price:7.45,
          options: {
              option1:
                   {
@@ -475,7 +475,7 @@ const MEE_TU = {
                       choices: [ "正常冰", "少冰", "去冰"], selectedOptions: [], limit: 1
                   },
              }},
-        { id: 835, name: '青梅綠茶(L)', price:7.25,
+        { id: 835, name: '青梅綠茶(L)', price:7.75,
          options: {
              option1:
                   {
@@ -500,7 +500,7 @@ const MEE_TU = {
                       choices: [ "正常冰", "少冰", "去冰"], selectedOptions: [], limit: 1
                   },
               }},
-        { id: 836, name: '鳳梨綠茶(L)', price:7.75,
+        { id: 836, name: '楊枝甘露(L)', price:8.99,
          options: {
              option1:
                   {
@@ -525,7 +525,7 @@ const MEE_TU = {
                       choices: [ "正常冰", "少冰", "去冰"], selectedOptions: [], limit: 1
                   },
               }},
-        { id: 837, name: '青提水果茶(L)', price:7.99,
+        { id: 837, name: 'Pina Colada (L)', price:8.45,
          options: {
              option1:
                   {
@@ -550,7 +550,7 @@ const MEE_TU = {
                       choices: [ "正常冰", "少冰", "去冰"], selectedOptions: [], limit: 1
                   },
               }},
-        { id: 838, name: '石榴莓莓(L)', price:7.95,
+        { id: 838, name: '石榴莓莓(L)', price:8.45,
          options: {
              option1:
                   {
@@ -575,7 +575,7 @@ const MEE_TU = {
                       choices: [ "正常冰", "少冰", "去冰"], selectedOptions: [], limit: 1
                   },
               }},
-        { id: 839, name: '茉莉甜橙(L)', price:7.75,
+        { id: 839, name: '茉莉甜橙(L)', price:8.25,
          options: {
              option1:
                   {
@@ -600,7 +600,7 @@ const MEE_TU = {
                       choices: [ "正常冰", "少冰", "去冰"], selectedOptions: [], limit: 1
                   },
               }},
-        { id: 849, name: '白蘭蔓越莓(L)', price:7.25,
+        { id: 849, name: '多肉芒果(L)', price:8.25,
          options: {
              option1:
                   {
