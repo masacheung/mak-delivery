d47707811421d3e24079dc09c8f352929b384711
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2025-09-11 22:07:30 -0400

    Update All Blue Chinese Cuisine.js

--- static/src/constant/restaurants/All Blue Chinese Cuisine.js
+++ static/src/constant/restaurants/All Blue Chinese Cuisine.js
@@ -1,1 +1,10 @@
-
+const All Blue Chinese Cuisine = {
+    id: 6,
+    name: '四海',
+    dishes: [
+        { id: 61, name: '魚頭泡餅 Fish Head Pot with Pancakes ', price:33.99  },
+        { id: 691, name: '酸菜全鱼 Fresh Fish Pot with Sauerkraut', price:38.99 },
+        { id: 692, name: '水煮全鱼 Spicy Whole fish in hot chill oil', price:38.99 },
+        { id: 652, name: '水煮魚片（黑魚）Sizzling fish in Chilli oil(black fish)', price:22.99  },
+        { id: 6116, name: '金汤椒麻鱼(黑鱼) Golfen Fish Fillet Soup', price:22.99 },
+        { id: 693, name: '一特色羊蝎子 Special lamb spine hotpot', price:38.99 ,
