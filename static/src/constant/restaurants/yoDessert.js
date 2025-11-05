05cd21be4d41fb1682f20dfc1e5495e89816ea70
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2025-11-04 22:03:40 -0500

    Update yoDessert.js

--- static/src/constant/restaurants/yoDessert.js
+++ static/src/constant/restaurants/yoDessert.js
@@ -2,19 +2,43 @@ const YO_DESSERT_US = {
 	id: 14,
 	name: 'Yo Dessert us',
 	dishes: [
-    { id: 1423, name: 'Kale Cheese Yogurt', price:7.25,
+    { id: 1423, name: 'Avocado Yogurt Smoothie', price:7.75,
 	options: {
         	option1: {
             	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
         	}
     	}},
+		{ id: 1413, name: 'Taro-ube Cheese Yogurt', price:7.75 ,
+    	options: {
+        	option1: {
+            	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
+        	}
+        }},
     { id: 1401, name: 'Mango Cheese Yogurt', price:6.25 ,
     	  options: {
         	option1: {
             	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
         	}
     	}},
-     { id: 1402, name: 'Peach Cheese Yogurt', price:6.95 ,
+		 { id: 1411, name: 'Oatmeal Yogurt smoothie', price:6.25 ,
+    	options: {
+        	option1: {
+            	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
+        	}
+        }},
+		{ id: 1412, name: 'Chestnut Popping Yogurt Smoothie', price:6.25 ,
+    	options: {
+        	option1: {
+            	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
+        	}
+        }},
+		   { id: 1409, name: 'Oreo Yogurt Smoothie', price:6.25 ,
+    	options: {
+        	option1: {
+            	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
+        	}
+        }},
+     { id: 1402, name: 'Peach Cheese Yogurt', price:6.25 ,
         options: {
             option1: {
                 name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
@@ -56,49 +80,28 @@ const YO_DESSERT_US = {
             	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
         	}
         }},
-    { id: 1409, name: 'Blueberry Cheese Yogurt', price:6.25 ,
-    	options: {
-        	option1: {
-            	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
-        	}
-        }},
+ 
     { id: 1410, name: 'Plain Cheese Yogurt', price:6.25 ,
     	options: {
         	option1: {
             	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
         	}
         }},
-    { id: 1411, name: 'Blackpink Cheese Yogurt', price:6.95 ,
-    	options: {
-        	option1: {
-            	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
-        	}
-        }},
-    { id: 1412, name: 'Swole-Cado Cheese Yogurt', price:7.25 ,
-    	options: {
-        	option1: {
-            	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
-        	}
-        }},
-    { id: 1413, name: 'Taro-ube Cheese Yogurt', price:7.25 ,
-    	options: {
-        	option1: {
-            	name: "Sugar level", choices: ["Normal", "Less"], selectedOptions: [], limit: 1
-        	}
-        }},
+
+
     { id: 1414, name: 'Oolong tea Soy Milk', price:3.95  },
-    { id: 1415, name: 'Homemade Milk Coffee', price:3.95  },
-    { id: 1416, name: 'Camellia Soy Milk', price:3.95  },
+	{ id: 1427, name: 'Camellia Soy Milk', price:3.25  },
+    { id: 1415, name: 'Homemade Milk Coffee', price:1.95  },
+    { id: 1416, name: 'Camellia Lemon Iced tea', price:6.95  },
     { id: 1417, name: 'Original Tiramisu', price:7.45  },
    	{ id: 1418, name: 'Matcha Tiramisu', price:7.95  },
     { id: 1419, name: 'Oreo Tiramisu', price:7.95  },
   	{ id: 1420, name: 'Strawberry Tiramisu', price:7.95  },
-    { id: 1421, name: 'Blueberry Tiramisu', price:7.95  },
+    { id: 1421, name: 'Taro Tiramisu', price:7.95  },
     { id: 1422, name: 'Original Egg Tart', price:2.95  },
-    
-    { id: 1424, name: 'Peral Choco Egg Tart', price:3.25  },
-    { id: 1425, name: 'Tiramisu Egg Tart', price:3.25  },
-     { id: 1426, name: 'Corn Egg Tart', price:3.25  } 
+    { id: 1424, name: 'Purple Rice Egg Tart', price:3.25  },
+    { id: 1425, name: 'Chocolate Egg Tart', price:3.25  },
+    { id: 1426, name: 'Corn Egg Tart', price:3.25  },
 	
   ]
 };
